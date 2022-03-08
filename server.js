236b14c411b40b6af49444439c1e3965ed45a8ff
Cocosmos <mipam.guillot@crea-inseec.com>
2022-03-08 20:18:37 +0100

    change the method for more efficience

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const alphabet = "abcdefghijklmnopqrstuvwxyz ";
 const size = alphabet.length;
 const max = 4;
 let start = new Date();
-let finish = new Date();
 let time = 0;
 let method = "";
 let guess = "";
@@ -31,12 +30,27 @@ const calculateHash = (str) => {
   return Buffer.from(hash).toString("base64");
 };
 
-/*****Call the bruteforces function *****/
-random_method();
-loop_method();
+/******Bruteforce password method 1******/
+//It will sequentially test all possibilities for 4 letters
+const forin_method = () => {
+  start = new Date();
+  const bruteforce = (g, num) => {
+    method = "For In";
+    if (num <= max) {
+      for (letter in alphabet) {
+        guess = g + alphabet[letter];
+        guess_crypted = calculateHash(guess);
+        bruteforce(guess, num + 1);
+        display();
+      }
+    }
+  };
+  bruteforce("", 1);
+};
 
-/******Bruteforce password method 1*****/
-function random_method() {
+/******Bruteforce password method 2*****/
+//It will randomly test all possibilities for 4 letters
+const random_method = () => {
   start = new Date();
   method = "Random";
   while (guess_crypted !== password) {
@@ -50,44 +64,27 @@ function random_method() {
       break;
     }
   }
-}
-
-/******Bruteforce password method 2******/
-//It will sequentially test all possibilities for 4 letters
-function loop_method() {
-  start = new Date();
-  method = "Multiple loop";
-  for (let letter in alphabet) {
-    for (let letter2 in alphabet) {
-      for (let letter3 in alphabet) {
-        for (let letter4 in alphabet) {
-          guess =
-            alphabet[letter] +
-            alphabet[letter2] +
-            alphabet[letter3] +
-            alphabet[letter4];
-          guess_crypted = calculateHash(guess);
-          display();
-        }
-      }
-    }
-  }
-}
+};
 
 /*****Function to display the solution*****/
 function display() {
   if (guess_crypted === password) {
     /*Calculate the time to perform the calculation*/
-    finish = new Date();
+    let finish = new Date();
     time = finish - start;
 
     console.log(
       `The encrypted password: "${guess_crypted}" was decrypted to: "${guess}" with the "${method}" method and it took: ${time} ms to perform. \n`
     );
   } else {
+    /*It can take a lot of time to show all possibilities with the random method*/
     //console.log("Password crypted: " + guess_crypted);
     //console.log("Password decrypted: " + guess + "\n");
   }
 }
+
+/*****Call the bruteforces function *****/
+forin_method();
+random_method();
 
 app.listen(port);
